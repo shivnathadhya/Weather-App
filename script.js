0c5629e0e8b3711d33080be59aeab8e45020d1f6
Shivnath Adhya <84401711+shivnathadhya@users.noreply.github.com>
2022-12-08 12:31:37 +0530

    commit1

---
+++ script.js
@@ -0,0 +1,89 @@
+/**
+ * Weather App
+ * TODO: Complete getWeatherData() to return json response Promise
+ * TODO: Complete searchCity() to get user input and get data using getWeatherData()
+ * TODO: Complete showWeatherData() to set the data in the the html file from response
+ */
+
+/* DIV ID's you'll need access to 👇
+"city-name"
+"weather-type"
+"temp"
+"min-temp"
+"max-temp"
+*/
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "01614f72dbmshc03295bb172b302p1f859djsne13a660b81af",
+    "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com",
+  },
+};
+
+// fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=kolkata&format=json&u=c', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+// const cityInput = document.getElementById("city-input");
+
+/**
+ * Retrieve weather data from openweathermap
+ * HINT: Use fetch()
+ * HINT: URL should look like this:
+ * https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=4f659b69081eb387321756a3c36d9853&units=metric
+ */
+// const apikey = "4f659b69081eb387321756a3c36d9853";
+const getWeatherData = (city) => {
+  //HINT: Use template literals to create a url with input and an API key
+  //CODE GOES HERE
+  const url = "https://yahoo-weather5.p.rapidapi.com/weather";
+  const fullurl = `${url}?location=${city}&format=json&u=c`;
+  const weatherPromise = fetch(fullurl, options);
+  return weatherPromise.then((response) => {
+    return response.json();
+  });
+};
+
+/**
+ * Retrieve city input and get the weather data
+ * HINT: Use the promise returned from getWeatherData()
+ */
+const searchCity = async () => {
+  let city = document.getElementById("city-input").value;
+  // CODE GOES HERE
+  console.log(city);
+  getWeatherData(city)
+    .then((data) => {
+      console.log(data);
+      showWeatherData(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const entersearchCity = (event) => {
+  if (event.keyCode === 13) {
+    searchCity();
+  }
+};
+
+/**
+ * Show the weather data in HTML
+ * HINT: make sure to console log the weatherData to see how the data looks like
+ */
+const showWeatherData = (weatherData) => {
+  //CODE GOES HERE
+  document.getElementById("city-name").innerText = weatherData.location.city;
+  document.getElementById("weather-type").innerText =
+    weatherData.current_observation.condition.text;
+  document.getElementById("temp").innerText =
+    weatherData.current_observation.condition.temperature;
+  document.getElementById("min-temp").innerText = weatherData.forecasts[0].low;
+  document.getElementById("max-temp").innerText = weatherData.forecasts[0].high;
+};
+
+document
+  .getElementById("city-input")
+  .addEventListener("keypress", entersearchCity);
